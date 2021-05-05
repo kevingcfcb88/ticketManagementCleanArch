f34fedbcfbd6ac40d6a3e2d23ef5605604774a87
Kevin Garay <kevingcfcb88@gmail.com>
2021-05-04 23:06:19 -0600

    Adding Application Core

---
+++ Ticket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticket.TicketManagement.Domain.Entities;
+
+namespace Ticket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+
+    }
+}
---
+++ Ticket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticket.TicketManagement.Domain.Entities;
+
+namespace Ticket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+    }
+}
