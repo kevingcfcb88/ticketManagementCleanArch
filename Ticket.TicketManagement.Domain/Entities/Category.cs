cb5b09242971b996aa9cedf1759f9bf214f3a27a
Kevin Garay <kevingcfcb88@gmail.com>
2021-05-04 22:43:43 -0600

    Add project files.

---
+++ Ticket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticket.TicketManagement.Domain.Common;
+
+namespace Ticket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Ticket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticket.TicketManagement.Domain.Common;
+
+namespace Ticket.TicketManagement.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
